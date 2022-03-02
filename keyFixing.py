92469b772c49ea9c98bef2453a8b187c575cf7c6
Diego G <dcgaray01@gmail.com>
2022-03-01 21:06:46 -0800

    finished part3
    this was alright, but the report will suck

--- keyFixing.py
+++ keyFixing.py
@@ -16,12 +16,9 @@ def task3():
 	plaintext = decrypt(ciphertext,keyTup[1])
 	print(plaintext)
 
-	if iMsg == plaintext:
-		print("ayo")
-
-
-
-
+	IV = get_random_bytes(16)
+	recoveredMsg =keyFixing(keyTup[0], keyTup[1], msg, IV)
+	print(recoveredMsg)
 
 def generateKeys(bit):
 	p1 = getPrime(bit)
@@ -40,3 +37,45 @@ def encrypt(msg, pu):
 
 def decrypt(encMsg, pr):
 	return pow(encMsg, pr[0], mod=pr[1])
+
+def keyFixing(pu, pr, msg, iv):
+
+	cPrime = pu[1]
+
+	secKey = pow(cPrime, pr[0], mod=pr[1])
+	secKey = secKey.to_bytes(128, "little")
+	hashThingy = SHA256.new()
+	hashThingy.update(secKey)
+	digest = hashThingy.hexdigest()	
+	intDigest = int(digest, 16)
+	byteKey = intDigest.to_bytes(35, "little")
+	key = byteKey[:16]
+
+	enc = AES.new(key, AES.MODE_CBC, iv)
+	bMsg = bytes(msg, "utf-8")
+	cNought = enc.encrypt(pad(bMsg, 16))
+
+	hashThingy2 = SHA256.new()
+	secKey2 = 0
+	secKey2 = secKey2.to_bytes(128, "little")
+	hashThingy2.update(secKey2)
+	digest = hashThingy2.hexdigest()
+	intDigest2 = int(digest, 16)
+	byteKey2 = intDigest2.to_bytes(35, "little")
+	key2 = byteKey2[:16]
+	enc2 = AES.new(key2, AES.MODE_CBC, iv)
+
+	pText = enc2.decrypt(cNought)
+	pText = unpad(pText, 16)
+	plaintext = pText.decode("utf-8") 
+
+	return plaintext
+
+
+
+
+
+
+
+
+

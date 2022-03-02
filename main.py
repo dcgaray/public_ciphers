c8ccf23b0dc950667ce6adf3b0401c2eef6cfd45
Diego G <dcgaray01@gmail.com>
2022-03-01 17:51:25 -0800

    symmetric key generation done
    okay, we're getting somwhere

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+import random,secrets
 from user import User
 
 def main():
+    '''
     you = "World"
     print(f"Hello {you}")
     g = User(2,3,4, "Alice")
+    g.genSecretKey(5)
     g.whoami()
+    '''
+    task1()
 
+def task1():
+    IV = secrets.token_bytes(16)
+    p = 37 
+    g = 5
+    usr1 = User(p,g,IV, "Alice")
+    print(f"I am {usr1.name}, with Priv: {usr1.priv}")
+    print(f"I am {usr1.name}, with Pub: {usr1.pub}")
+    usr2 = User(p,g,IV, "Bob")
+    print(f"I am {usr2.name}, with Priv: {usr2.priv}")
+    print(f"I am {usr2.name}, with Pub: {usr2.pub}")
+    usr1.genSecretKey(usr2.pub)
+    usr2.genSecretKey(usr1.pub)
+
+    usr1.genSymmetricKey() 
+    usr2.genSymmetricKey() 
+    print(usr1.symKey)
+    print(usr2.symKey)
+
+    if usr1.symKey == usr2.symKey:
+        print('they got the same key')
+    if usr1.symKey == 0 and usr2.symKey == 0:
+        print("they're both zero")
 
 if __name__ == "__main__":
     main()
--- user.py
+++ user.py
@@ -2,12 +2,34 @@ import random
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 
+
+
 class User():
     def __init__(self, p, g, initialVector, username):
-        self.p = p
-        self.g = g
+        self.priv = random.randint(0, p-1)
+        self.pub = pow(g, self.priv, p) 
         self.name = username
-        self.IV = initialVector
+        self.IV = initialVector #IV variable
+        self.secKey = None #var for da secret key
+        self.symKey = None #var for symmetric key
+        self.encryKey = None
+
+    #generates a secret key used for symmetric encryption from another
+        # user's public key
+    def genSecretKey(self, pubKey):
+        # A = g^a % p
+        # g = pubInput, a = self.priv, p = self.pub
+        self.secKey = pow(pubKey, self.priv, self.pub) 
+
+    #performs SHA256 hashing and gets a 2byte symmetric key for AES-CBC
+    def genSymmetricKey(self):
+        bOrder = "little" #byteOrder mode necessary for Python's "to_bytes func"
+        hashThingy = SHA256.new()
+        hashThingy.update(self.secKey.to_bytes(128, bOrder))
+        digest = hashThingy.hexdigest()
+        intDigest = int(digest, 16) #get our hex digest into a hex integer
+        byteKey = intDigest.to_bytes(35, bOrder)
+        self.symKey = byteKey[:16]
 
     def whoami(self):
-        print(self.name)
+        print(f"I am {self.name}: {self.secKey}")

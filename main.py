05eae20dbdc4479320a96a0c1b7d8e33789a2733
Diego G <dcgaray01@gmail.com>
2022-03-07 21:25:50 -0800

    added comments
    okay, so tasks1-2 seem to be done

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
     you = "World"
     print(f"Hello {you}")
     '''
+    #This is the intial message passed
     p = int("B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371", 16) 
+    #In class call g either q or alpha, which makes our lives harder...
     g = int("A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5", 16)
     
     task1(37,5)
@@ -22,14 +24,19 @@ def main():
 
 def task1(p,g):
     IV = secrets.token_bytes(16)
+    #usingo our good ole classic alice and bob
     usr1 = User(p,g,IV, "Alice")
     usr2 = User(p,g,IV, "Bob")
+
+    #now that we've generated publive keys, we need to generate secret keys using the other's public key
     usr1.genSecretKey(usr2.pub)
     usr2.genSecretKey(usr1.pub)
 
+    #create a symmetric key using the secret key to share information
     usr1.genSymmetricKey() 
     usr2.genSymmetricKey()
 
+    #start sharing information using the symmetric key
     encryptedMsg1 =usr1.encrypt("I am a secret")
     encryptedMsg2 =usr2.encrypt("I am also a secret")
     #^^^ First half of Task1 ^^^#
--- user.py
+++ user.py
@@ -15,8 +15,8 @@ class User():
         self.IV = initialVector #IV variable
         self.secKey = None #var for da secret key
         self.symKey = None #var for symmetric key
-        self.encryKey = None
-        self.bOrder = "little" #byteOrder mode necessary for Python's "to_bytes func"
+        self.byteOrder = "little" #byteOrder mode necessary for Python's "to_bytes func"
+        self.blckLen = 16
 
     #generates a secret key used for symmetric encryption from another
         # user's public key
@@ -25,30 +25,35 @@ class User():
         # g = pubInput, a = self.randElem, p = self.p 
         self.secKey = pow(pubKey, self.randElem, self.p) 
 
+    #normally this could be done right after the generation of the secret key
+        #but in light of the assignment specifications, this is done in multiple steps
     #performs SHA256 hashing and gets a 2byte symmetric key for AES-CBC
     def genSymmetricKey(self):
-        hashThingy = SHA256.new()
-        hashThingy.update(self.secKey.to_bytes(128, self.bOrder))
-        digest = hashThingy.hexdigest()
-        intDigest = int(digest, 16) #get our hex digest into a hex integer
-        byteKey = intDigest.to_bytes(35, self.bOrder)
-        self.symKey = byteKey[:16]
+        self.blckLen = 16
+        hashObj = SHA256.new()
+        hashObj.update(self.secKey.to_bytes(128, self.byteOrder))
+        digest = hashObj.hexdigest()
+        intDigest = int(digest, self.blckLen) #get our hex digest into a hex integer
+        byteDigest = intDigest.to_bytes(35, self.byteOrder)
+        self.symKey = byteDigest[:self.blckLen]
 
     #takes a messsage and encrypts it using AES-CBC
     def encrypt(self, msg):
-        blckLen = 16 
+        self.blckLen = 16 
         enc = AES.new(self.symKey, AES.MODE_CBC, self.IV)
         plaintext = bytes(msg, "utf-8")
         ptLen = len(plaintext)
+        blkRemainder = ptLen % self.blckLen
 
-        #check to see if we have to #PCKS 7 pad it
-        if (ptLen % blckLen) != 0:
-            padLen = blckLen - (ptLen % blckLen)
+        #pad the information using #PCKS 7 standards
+        if blkRemainder != 0:
+            padLen = self.blckLen - blkRemainder 
+            #check to see how much we need to pad our block by
             for idx in range(padLen):
-                plaintext += idx.to_bytes(1, self.bOrder)
-        #no padding necessary 
+                plaintext += idx.to_bytes(1, self.byteOrder)
+        #if our message is the size of the block, pad it with an extra block anyways
         else:
-            for i in range(blckLen):
+            for i in range(self.blckLen):
                 plaintext += b"16"
 
         return enc.encrypt(plaintext)
@@ -56,21 +61,24 @@ class User():
     #takes an AES-CBC encrypted messages and returns the plaintext
     def decrypt(self, ciphertext):
         enc = AES.new(self.symKey, AES.MODE_CBC, self.IV)
-        bText = enc.decrypt(ciphertext) # this is in bytes
-        bLen = len(bText)
+        byteText = enc.decrypt(ciphertext) # this is in bytes
+        bLen = len(byteText)
+
+        #go through and find out where our padding occurs within our encrypted message
         nonPadCount = 0
+        #starting at the end and workingo our down to zero
         for i in range(bLen-1, bLen, -1):
             # loop through our bytes until we find out where our padding occurs
-            if bText[bLen - 1] ==  bText[i]:
+            if byteText[bLen - 1] ==  byteText[i]:
                 j += 1
             #PCKS 7 dictates the padding be the number of bytes remaining for pad
             else: 
                 break
 
         #remove our padding
-        if nonPadCount == bText[bLen - 1]:
-            bText = bText[:bLen - nonPadCount]
-        return bText.decode("utf-8")
+        if nonPadCount == byteText[bLen - 1]:
+            bText = byteText[:bLen - nonPadCount]
+        return byteText.decode("utf-8")
 
     #returns the username provided when the user was first created
     def whoami(self):
